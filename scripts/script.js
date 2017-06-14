9e1601310a43d42d95b1eeab4668908659ffe044
leibalex <leibalex91@gmail.com>
2017-06-15 01:01:10 +0300

    Добавил обрабочтик на добавление таска в список дел

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,42 @@
-let doc = document,
-	add = doc.getElementById('add');
 
-add.onclick = ()=>{
-	let text = doc.getElementById('text'),
-		li = doc.createElement('li'),
-		doneButton = doc.createElement("<input type ='button' class = 'done'>"),
-		cancellButton = doc.createElement("<input type ='button' class = 'cancell'>"),
-		span = doc.createElement('span'),
-		ul = doc.querySelector('ul');
+window.onload = ()=>{
 
-	span.innerHTML = text;
-	cancellButton.
-	li.appendChild(doneButton);
-	li.appendChild(span);
-	li.appendChild(cancellButton);
-	ul.appendChild(li);
+	let doc = document,
+		add = doc.getElementById('add'),
+		ul = doc.querySelector('ul'),
+		doneButtons = doc.querySelectorAll('.done'),
+		cancellButtons = doc.querySelectorAll('.cancell');
+
+	add.onclick = ()=>{
+		let text = doc.getElementById('text').value;
+
+		if(!text){
+			alert('Поле не может быть пустым');
+			return;
+		}
+
+		let li = doc.createElement('li'),
+			doneButton = doc.createElement('input'),
+			cancellButton = doc.createElement('input'),
+			span = doc.createElement('span');
+			
+
+		doneButton.setAttribute('type','button');
+		cancellButton.setAttribute('type','button');
+
+		doneButton.classList.add('done');
+		cancellButton.classList.add('cancell');
+
+		span.innerHTML = text;
+		li.appendChild(doneButton);
+		li.appendChild(span);
+		li.appendChild(cancellButton);
+		ul.appendChild(li);
+
+		doc.getElementById('text').value = '';
+	}
+
 }
-
-
 
 
 
